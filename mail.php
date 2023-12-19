d3e7339c4e39efa3ade099e742613e36dfce7716
WuorBang <uhuribhang21@gmail.com>
2023-12-19 23:22:43 +0300

    form

--- mail.php
+++ mail.php
@@ -2,26 +2,19 @@
 // Only process POST requests.
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the form fields and remove whitespace.
-    $name = strip_tags(trim($_POST["Name"]));
+    $name = strip_tags(trim($_POST["name"]));
     $name = str_replace(array("\r","\n"),array(" "," "),$name);
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
     $subject = trim($_POST["subject"]);
     $message = trim($_POST["message"]);
 
-    // Check that data was sent to the mailer.
-    if (empty($name) || empty($subject) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        // Set a 400 (bad request) response code and exit.
-        http_response_code(400);
-        echo "Please complete the form and try again.";
-        exit;
-    }
 
+    // How to validate a form using Javscript?
+    
+  
     // Set the recipient email address.
     // FIXME: Update this to your desired email address.
     $recipient = "uhuribhang211@gmail.com";
-
-    // Set the email subject.
-    $subject = "$subject";
 
     // Build the email content.
     $email_content = "Name: $name\n";
